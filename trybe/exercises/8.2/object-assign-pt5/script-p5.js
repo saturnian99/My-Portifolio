28918233196d4eda24a8e360515de7037dfe9793
saturnian99 <saturnian.dsg@gmail.com>
2020-03-19 19:33:50 -0300

    Object.assign Practice - Part V: done exercise 5!

--- trybe/exercises/8.2/object-assign-pt5/script-p5.js
+++ trybe/exercises/8.2/object-assign-pt5/script-p5.js
@@ -1,5 +1,29 @@
-const fighter = {
-  earthlingName: "Son Goku",
-  powerLevel: "8000+"
+const saiyan = {
+  name: "kakarot",
+  powerLevel: "8000+",
 }
 
+const kaioshin = {
+  name: "Zamasu",
+  side: "Evil",
+}
+
+const gokuBlack = Object.assign(saiyan, kaioshin); // With this method, I can access and manipulate the original object "Goku" using another object "Zamasu"
+
+console.log(saiyan);
+console.log(gokuBlack);
+
+const saiyanPrince = {
+  name: "Vegeta",
+  powerLevel: "7999",
+}
+
+const miracleWater = {
+  name: "Commeson",
+  color: "Pink",
+}
+
+const fakeVegeta = Object.assign({}, saiyanPrince, miracleWater); // Here, I've "cloned" the two objects into one, manipulating the clone "Fake Vegeta" without altering the original "Vegeta"
+
+console.log(saiyanPrince);
+console.log(fakeVegeta);
