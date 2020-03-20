bb956bffede4262557522ad0ba17598e96335571
saturnian99 <saturnian.dsg@gmail.com>
2020-03-20 15:49:25 -0300

    Objects Exercises: doing exercise 8 (verify pair key-value existence in object)

--- trybe/exercises/8.2/objects-exercises/script.js
+++ trybe/exercises/8.2/objects-exercises/script.js
@@ -89,3 +89,17 @@ getValueByIndex(lesson2, 0);
 8) Crie uma função que verifique se o par (chave / valor) existem na função. 
 Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 */
+
+const verifyPair = (object, key, value) => {  
+  const pair = [ key, value ];
+
+  for (i in Object.entries(object)) {
+    console.log(Object.entries(object)[i]);
+    if (Object.entries(object)[i] == pair) {
+      console.log("Existe!");
+    }
+  }
+}
+
+console.log("\n========== Exercise 8 - Verify pair key-value existence in object ==========\n");
+verifyPair(lesson1, "materia", "Matemática");
