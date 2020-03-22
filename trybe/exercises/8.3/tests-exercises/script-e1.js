24ace510ad8a97e66878ae9316ca2e1846827145
saturnian99 <saturnian.dsg@gmail.com>
2020-03-22 14:39:05 -0300

    Unit Testing Exercise 1: Done (3/4)

--- trybe/exercises/8.3/tests-exercises/script-e1.js
+++ trybe/exercises/8.3/tests-exercises/script-e1.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
 
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("parameters must be numbers");
   }
-
+  
   return a + b;
 }
 
-// implemente seus testes aqui
+/*
+1.1) Teste se o retorno de sum(4, 5) é 9;
+1.2) Teste se o retorno de sum(0, 0) é 0;
+1.3) Teste se a função 'sum' lança um erro quando os parametros são 4 e "5"(string 5);
+1.4) Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5").
+*/
+
+assert(sum(4, 5) == 9 , "9 == 9");
+assert(sum(0, 0) == 0, "0 == 0");
+assert.throws(() => { sum(4, "5") });
