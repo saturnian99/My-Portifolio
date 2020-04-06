0ee54155c6250127cd8708db6e3a1a9b36aabaf3
saturnian99 <saturnian.dsg@gmail.com>
2020-04-06 19:52:28 -0300

    HOF exercise 8 done!

--- trybe/exercises/9.1/hof-e8.js
+++ trybe/exercises/9.1/hof-e8.js
@@ -65,55 +65,40 @@ const books = [
 
 const expected_result = [
   {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
 ]
 
-function fantasyOrScienceFiction(objects) {
-  let array = [];
-  objects.filter(object => {
-    if (object.genre == 'Ficção Científica' || object.genre == 'Fantasia') {
-      array.push(object);
-    };
+function oldBooks(objects) {
+  let array = objects.filter(object => 2020 - object.releaseYear > 60);
+  array.sort((a, b) => {
+    if (a.releaseYear > b.releaseYear) {
+      return 1;
+    } if (a.releaseYear < b.releaseYear) {
+      return -1;
+    } else {
+      return 0;
+    }
   });
   return array;
 }
 
-assert.deepEqual(fantasyOrScienceFiction(books), expected_result);
+assert.deepEqual(oldBooks(books), expected_result);
