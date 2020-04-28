76fadb5163bb5ad1199d01b1a71926a5b51a7c95
saturnian99 <saturnian.dsg@gmail.com>
2020-04-28 15:54:09 -0300

    Created new 'tasklist' React App; Implemented function to add task to list

---
+++ trybe/exercises/12.1/tasklist-app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const tasks = ["Organize bed", "Drink water", "Meditate"];
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. {tasks.map(element => task(element))}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
