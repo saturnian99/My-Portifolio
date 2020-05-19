b870e930f99efbdf321978307f5110c3a5b459c5
saturnian99 <saturnian.dsg@gmail.com>
2020-05-19 18:59:54 -0300

    Implemented error handler on 'sendMarsTemperature' function at 4-mars exercise

--- trybe/exercises/10.2/exercises-10-2/4-mars-temperature.js
+++ trybe/exercises/10.2/exercises-10-2/4-mars-temperature.js
@@ -9,12 +9,22 @@ const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-const sendMarsTemperature = (callback) => {
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (onSucess, onFailure) => {
+  const failure = Math.random() >= 0.6;
   const temperature = getMarsTemperature();
-  if (callback) return callback(temperature);
+  if (onFailure && failure) return onFailure("Robot is busy")
+  if (onSucess) return onSucess(temperature);
   console.log(`Mars temperature is: ${temperature}º Celsius`);
 }
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
