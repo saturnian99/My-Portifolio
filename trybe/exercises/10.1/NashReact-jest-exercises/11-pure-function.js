2bcdc9ed9eedd1d101999405cf940a07cd531b06
saturnian99 <saturnian.dsg@gmail.com>
2020-05-27 15:22:21 -0300

    Implemented 'pure' function

--- trybe/exercises/10.1/NashReact-jest-exercises/11-pure-function.js
+++ trybe/exercises/10.1/NashReact-jest-exercises/11-pure-function.js
@@ -1,11 +1,15 @@
 // setPrice(item: Object, price: Number) => item: Object
 const setPrice = (item, price) => {
-  // TODO: implement
+  const copy = Object.assign({}, item);
+  copy.price = price;
+  return copy;
 };
 
 // addToCart(cart: Array, item: Object) => cart: Array
 const addToCart = (cart, item) => {
-  // TODO: implement
+  const copy = cart.slice();
+  copy.push(item);
+  return copy;
 };
 
 module.exports = {setPrice, addToCart};
