2e68a493f0dd9f15c2554c869158263a426e2920
saturnian99 <saturnian.dsg@gmail.com>
2020-05-28 19:16:37 -0300

    Created HTML page; Created JS app to fetch bad jokes using promises

--- trybe/exercises/10.3/exercises/bad-jokes-gen/script.js
+++ trybe/exercises/10.3/exercises/bad-jokes-gen/script.js
@@ -1,7 +1,33 @@
+const jokeContainer = document.getElementById('jokeContainer');
 const API_URL = 'https://icanhazdadjoke.com/';
-
-const fetchJoke = () => {
-  // Adicionar lógica aqui!
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
 };
+
+fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => jokeContainer.innerHTML = data.joke);
+
+const isOverEightThousand = () => {
+  return new Promise((resolve, reject) => {
+    let sum = 0;
+    for (i = 0; i < 10; i++) {
+      sum += Math.random() * 2500;
+    }
+
+    if (sum < 80000) {
+      resolve(
+        [sum / 2, sum / 3, sum / 5, sum / 10]
+      );
+    }
+    reject('É mais de oito mil! Essa promise deve estar quebrada!')
+  });
+};
+
+isOverEightThousand()
+  .then((result) => result)
+  .then((arr) => console.log(arr.reduce((acc, num) => acc + num, 0)))
+  .catch((message) => console.log(`Error: ${message}`));
 
 window.onload = () => fetchJoke();
