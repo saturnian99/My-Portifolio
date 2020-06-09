904d680546d51a931a0675df0e7abfd9ee6d2cd2
saturnian99 <saturnian.dsg@gmail.com>
2020-06-09 18:50:59 -0300

    Created 'NextButton' component

---
+++ trybe/exercises/12.1/my-pokedex/src/NextButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NextButton extends React.Component {
+  render() {
+    return (
+      <button>Próximo pokémon</button>
+    );
+  };
+};
+
+export default NextButton;
