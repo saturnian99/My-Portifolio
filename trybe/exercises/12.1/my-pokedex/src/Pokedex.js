1384969ac26bb2e1fd9e5dbaf63477a89e4da15b
saturnian99 <saturnian.dsg@gmail.com>
2020-06-10 20:05:44 -0300

    Implemented other buttons to cycle between different types of pokemons; When type changes, the first pokemon (of that type) appears by default

--- trybe/exercises/12.1/my-pokedex/src/Pokedex.js
+++ trybe/exercises/12.1/my-pokedex/src/Pokedex.js
@@ -7,24 +7,49 @@ class Pokedex extends React.Component {
     super(props);
     this.state = {
       index: 0,
+      pokemons: this.props.pokemons,
+      type: '',
     }
   }
 
-  changePokemon = () => {
+  changeIndex = (pokemons) => {
     const index = this.state.index;
-    const pokemons = this.props.pokemons;
-    if (index !== (pokemons.length - 1)) {
-      this.setState({ index: index + 1, });      
+    const lastIndex = pokemons.length - 1;
+    if (index < lastIndex) {
+      this.setState({ index: index + 1, });
     } else {
       this.setState({ index: 0, })
     }
   }
 
+  filterByType = (type) => {
+    let pokemons = this.props.pokemons;
+
+    if (type !== "All") {
+      pokemons = pokemons.filter(
+        pokemon => pokemon.type === type
+      );
+    }
+    
+    if (type !== this.state.type) {
+      // change to first pokemon of type when changing types (buttons)
+      this.setState(() => ({ index: 0, }))
+    } else {
+      // if it's still in the same type, continue cycling
+      this.changeIndex(pokemons); 
+    }
+
+    this.setState({ pokemons: pokemons, type, });
+ 
+  }
+
   render() {
     return (
       <section className="pokemon-viewer">
-        <Pokemon pokemon={this.props.pokemons[this.state.index]} />
-        <NextButton clickHandler={this.changePokemon} />
+        <Pokemon pokemon={this.state.pokemons[this.state.index]} />
+        <NextButton clickHandler={() => this.filterByType("All")} />
+        <NextButton clickHandler={() => this.filterByType("Fire")} />
+        <NextButton clickHandler={() => this.filterByType("Psychic")} />
       </section>
     );
   };
