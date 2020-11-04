2ed1bd92fdb3a6d70151782efb0bc316d941be92
saturnian99 <saturnian.dsg@gmail.com>
2020-11-04 16:49:06 -0300

    #1 practice exercise and some tests done

---
+++ trybe/exercises/25.1/practice.js
@@ -0,0 +1,73 @@
+// Aggregation Pipeline practice exercises:
+// 1) '$match' and '$limit' operators
+// creating 'transactions' collection first
+
+db.transactions.insertMany([
+  { value: 5900, from: "Dave America", to: "Ned Flanders", bank: 'International' },
+  { value: 1000, from: "Mark Zuck", to: "Edna Krabappel", bank: 'FloridaBank' },
+  { value: 209, from: "Lisa Simpson", to: "Dave America", bank: 'bankOfAmerica' },
+  { value: 10800, from: "Arnold Schuz", to: "Mark Zuck", bank: 'JPMorgan' },
+  { value: 850, from: "Barney Gumble", to: "Lisa Simpson", bank: 'Citigroup' },
+  { value: 76000, from: "Ned Flanders", to: "Edna Krabappel", bank: 'JPMorgan' },
+  { value: 1280, from: "Dave America", to: "Homer Simpson", bank: 'Citigroup' },
+  { value: 7000, from: "Arnold Schuz", to: "Ned Flanders", bank: 'International' },
+  { value: 59020, from: "Homer Simpson", to: "Lisa Simpson", bank: 'International' },
+  { value: 100, from: "Mark Zuck", to: "Barney Gumble", bank: 'floridaBank' },
+]);
+
+db.transactions.aggregate([
+  { $match: { from: "Dave America" } },
+  {
+    $match: {
+      $or: [
+        { value: { $gt: 700, $lt: 6000 } },
+        { to: "Lisa Simpson" }
+      ]
+    }
+  },
+  { $match: { value: { $gt: 1000 } } },
+  { $limit: 3 }
+]);
+
+// 'let' and 'pipeline' parameters test
+// creating two collections first
+
+db.orders.insertMany([
+  { _id: 1, item: "almonds", price: 12, ordered: 2 },
+  { _id: 2, item: "pecans", price: 20, ordered: 1 },
+  { _id: 3, item: "cookies", price: 10, ordered: 60 }
+]);
+
+db.warehouses.insertMany([
+  { _id: 1, stock_item: "almonds", warehouse: "A", instock: 120 },
+  { _id: 2, stock_item: "pecans", warehouse: "A", instock: 80 },
+  { _id: 3, stock_item: "almonds", warehouse: "B", instock: 60 },
+  { _id: 4, stock_item: "cookies", warehouse: "B", instock: 40 },
+  { _id: 5, stock_item: "cookies", warehouse: "A", instock: 80 }
+]);
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "warehouses",
+      let: { order_item: "$item", order_qty: "$ordered" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ["$stock_item", "$$order_item"] },
+                { $gte: ["$instock", "$$order_qty"] }
+              ]
+            }
+          }
+        },
+        { $project: { stock_item: 0, _id: 0 } }
+      ],
+      as: "stockdata"
+    }
+  }
+]).pretty();
+
+// 2) '$lookup' operator / 'let' and 'pipeline' parameters
+
